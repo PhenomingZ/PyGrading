49ff306e4a26e2d6264bb2127e4e769f43e81749
Charles Zhang <694556046@qq.com>
2020-01-20 23:54:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pygrading",
-    version="0.0.1",
+    version="0.0.2",
     author="Charles Zhang",
     author_email="694556046@qq.com",
     description="A Python ToolBox for CourseGrading platform.",
