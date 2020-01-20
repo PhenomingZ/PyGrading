e16f77a4387ae6710d59816efaacb9764467c3b8
Charles Zhang <694556046@qq.com>
2020-01-20 23:36:50 +0800

    Add gitignore and License

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyGrading",
+    version="0.0.1",
+    author="Charles Zhang",
+    author_email="694556046@qq.com",
+    description="A Python ToolBox for CourseGrading platform.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PhenomingZ/PyGrading",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
